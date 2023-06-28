d2f4989219c9cd947c4b0cec65cbb43a328794d6
BintusCal <cezerike@gmail.com>
2023-06-28 21:34:45 +0100

    commit

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Container } from 'react-bootstrap'
+import { Container, Row, Col, Image } from 'react-bootstrap'
 import { products_0 } from '../products'
 import Slideshow from '../components/Slideshow'
 
@@ -10,6 +10,38 @@ const HomeScreen = () => {
         <h1 className='text-header'>Welcome to Bintus Stores</h1>
         <Slideshow products_0={products_0} />
         <h2 className='text-header2'>Categories</h2>
+        <Row>
+          <Col xs={6} md={4} className='position-relative'>
+            <Image
+              src='/hoodies_images/IMG_0842.JPG'
+              className='img-fluid component-images'
+              thumbnail
+            />
+            <div className='position-absolute top-50 start-50 translate-middle text-center'>
+              <h3 className='text-light'>Hoodies</h3>
+            </div>
+          </Col>
+          <Col xs={6} md={4} className='position-relative'>
+            <Image
+              src='/tees_images/IMG_0898.JPG'
+              className='img-fluid component-images'
+              thumbnail
+            />
+            <div className='position-absolute top-50 start-50 translate-middle text-center'>
+              <h3 className='text-light'>Tees</h3>
+            </div>
+          </Col>
+          <Col xs={6} md={4} className='position-relative'>
+            <Image
+              src='/tees_images/IMG_0898.JPG'
+              className='img-fluid component-images'
+              thumbnail
+            />
+            <div className='position-absolute top-50 start-50 translate-middle text-center'>
+              <h3 className='text-light'>Hoodies</h3>
+            </div>
+          </Col>
+        </Row>
       </Container>
     </>
   )
