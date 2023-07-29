3dbc8464c6141f4326a360bf7178032b2bfd2804
Ezerike Caleb Chikere <cezerike@gmail.com>
2023-07-29 16:32:03 +0100

    'category screen'

--- src/screens/CatalogScreen.js
+++ src/screens/CatalogScreen.js
@@ -12,16 +12,23 @@ const CatalogScreen = () => {
         </div>
         <hr />
         <Row>
-          <div className='custom-margin'>
+          <div className='custom-margin' style={{ border: '2px solid red' }}>
             {categoryItems.map((categoryItem) => (
               <div
                 xs={6}
                 md={4}
-                className='position-relative row justify-content-end'
+                className='position-relative row justify-content-center'
                 style={
                   categoryItem._id % 2 === 0
-                    ? { order: 0, flexBasis: '250px', flexGrow: '2' }
-                    : { order: 0, flexBasis: 'auto' }
+                    ? {
+                        order: 0,
+                        flexBasis: '250px',
+                        flexGrow: '2',
+                      }
+                    : {
+                        order: 0,
+                        flexBasis: 'auto',
+                      }
                 }>
                 <Link to={`/categoryItem/${categoryItem._id}`}>
                   <Image
