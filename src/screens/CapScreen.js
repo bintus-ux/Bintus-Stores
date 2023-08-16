c9e61f4e0ad65204af54288cbfa1a4729db6226f
Ezerike Caleb Chikere <cezerike@gmail.com>
2023-08-16 23:06:20 +0100

    'implementing dispatch calls from store'

--- src/screens/CapScreen.js
+++ src/screens/CapScreen.js
@@ -1,20 +1,21 @@
 import React, { useState, useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { Row, Image } from 'react-bootstrap'
-import axios from 'axios'
+import Message from '../components/Message'
+import Loader from '../components/Loader'
+import { listCapItems } from '../actions/capActions'
 
 const CapScreen = () => {
-  const [capItems, setCapItems] = useState([])
+  const dispatch = useDispatch()
+
+  const capList = useSelector((state) => state.capList)
+  const { loading, error, capItems } = capList
 
   useEffect(() => {
-    const fetchCapItems = async () => {
-      const { data } = await axios.get('/api/categoryItems/caps')
+    dispatch(listCapItems())
+  }, [dispatch])
 
-      setCapItems(data)
-    }
-
-    fetchCapItems()
-  }, [])
   const isFound = capItems.some((cap) => {
     if (cap._id) {
       return true
@@ -26,76 +27,88 @@ const CapScreen = () => {
       <Link className='btn btn-light my-3' to='/'>
         Go Back
       </Link>
-      {isFound ? (
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant='danger'>{error}</Message>
+      ) : (
         <>
-          <div className='container my-5'>
-            <div className='row'>
-              <div className='col-12'>
-                <h2 className='display-4 text-center text-capitalize font-italic'>
-                  Caps
-                </h2>
-                <hr className='border border-primary ' />
-              </div>
-            </div>
-          </div>
-          <Row>
-            <div className='custom-margin'>
-              {capItems.map((capItem) => (
-                <div xs={6} md={4} key={capItem._id} className='text-center'>
-                  <Link
-                    to={`/categoryItems/${capItem.category}/${capItem._id}`}>
-                    {capItem.countInStock === 0 ? (
-                      <>
-                        <div className='row justify-content-left'>
-                          <div className='circle d-flex align-items-center justify-content-center'>
-                            <p className='circle-text position-absolute'>
-                              Sold
-                              <br /> Out
-                            </p>
-                          </div>
-                        </div>
-                        <Image
-                          src={capItem.image}
-                          className='img-fluid component-images lighter'
-                          style={{ height: '450px', width: 'auto' }}
-                        />
-                      </>
-                    ) : (
-                      <Image
-                        src={capItem.image}
-                        className='img-fluid component-images darker'
-                        style={{ height: '450px', width: 'auto' }}
-                      />
-                    )}
-                  </Link>
-
-                  <div>
-                    <Link
-                      to={`/categoryItems/${capItem.category}/${capItem._id}`}
-                      style={{ textDecoration: 'none' }}>
-                      <h3
-                        style={{ color: 'black' }}
-                        className='text-capitalize'>
-                        {capItem.name}
-                      </h3>
-                      <h3 style={{ color: 'black' }}>- ₦{capItem.price}</h3>
-                    </Link>
+          {isFound ? (
+            <>
+              <div className='container my-5'>
+                <div className='row'>
+                  <div className='col-12'>
+                    <h2 className='display-4 text-center text-capitalize font-italic'>
+                      Caps
+                    </h2>
+                    <hr className='border border-primary ' />
                   </div>
                 </div>
-              ))}
+              </div>
+              <Row>
+                <div className='custom-margin'>
+                  {capItems.map((capItem) => (
+                    <div
+                      xs={6}
+                      md={4}
+                      key={capItem._id}
+                      className='text-center'>
+                      <Link
+                        to={`/categoryItems/${capItem.category}/${capItem._id}`}>
+                        {capItem.countInStock === 0 ? (
+                          <>
+                            <div className='row justify-content-left'>
+                              <div className='circle d-flex align-items-center justify-content-center'>
+                                <p className='circle-text position-absolute'>
+                                  Sold
+                                  <br /> Out
+                                </p>
+                              </div>
+                            </div>
+                            <Image
+                              src={capItem.image}
+                              className='img-fluid component-images lighter'
+                              style={{ height: '450px', width: 'auto' }}
+                            />
+                          </>
+                        ) : (
+                          <Image
+                            src={capItem.image}
+                            className='img-fluid component-images darker'
+                            style={{ height: '450px', width: 'auto' }}
+                          />
+                        )}
+                      </Link>
+
+                      <div>
+                        <Link
+                          to={`/categoryItems/${capItem.category}/${capItem._id}`}
+                          style={{ textDecoration: 'none' }}>
+                          <h3
+                            style={{ color: 'black' }}
+                            className='text-capitalize'>
+                            {capItem.name}
+                          </h3>
+                          <h3 style={{ color: 'black' }}>- ₦{capItem.price}</h3>
+                        </Link>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </Row>
+            </>
+          ) : (
+            <div className='container my-5'>
+              <div className='row'>
+                <div className='col-12'>
+                  <p className='display-4 text-center font-italic'>
+                    Sorry, there are no available items in this collection.
+                  </p>
+                </div>
+              </div>
             </div>
-          </Row>
+          )}
         </>
-      ) : (
-        <div className='container my-5'>
-          <div className='row'>
-            <div className='col-12'>
-              <p className='display-4 text-center font-italic'>
-                Sorry, there are no available items in this collection.
-              </p>
-            </div>
-          </div>
-        </div>
       )}
     </>
   )
