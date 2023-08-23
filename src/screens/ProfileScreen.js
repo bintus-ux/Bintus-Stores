98de2c32ee3f2e54480378c9117216978b428c7e
Ezerike Caleb Chikere <cezerike@gmail.com>
2023-08-23 23:36:20 +0100

    'setting input fields in profile screen'

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -5,7 +5,11 @@ import { LinkContainer } from 'react-router-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
-import { getUserDetails, updateUserProfile } from '../actions/userActions'
+import {
+  register,
+  getUserDetails,
+  updateUserProfile,
+} from '../actions/userActions'
 
 const ProfileScreen = () => {
   const [name, setName] = useState('')
@@ -14,11 +18,11 @@ const ProfileScreen = () => {
   const [confirmPassword, setConfirmPassword] = useState('')
   const [message, setMessage] = useState(null)
 
-  let history = useNavigate()
+  let navigate = useNavigate()
 
   const dispatch = useDispatch()
 
-  const userDetails = useSelector((state) => state.userRegister)
+  const userDetails = useSelector((state) => state.userDetails)
   const { loading, error, user } = userDetails
 
   const userLogin = useSelector((state) => state.userLogin)
@@ -29,16 +33,17 @@ const ProfileScreen = () => {
 
   useEffect(() => {
     if (!userInfo) {
-      history.push('/login')
+      navigate('/login')
     } else {
-      if (!userInfo.name) {
+      if (!user.name) {
         dispatch(getUserDetails('profile'))
       } else {
-        setName(userInfo.name)
-        setEmail(userInfo.email)
+        setName(user.name)
+        setEmail(user.email)
       }
     }
-  }, [dispatch, userInfo, user])
+  }, [dispatch, navigate, userInfo, user])
+
   const submitHandler = (e) => {
     e.preventDefault()
     if (password !== confirmPassword) {
@@ -46,6 +51,26 @@ const ProfileScreen = () => {
     } else {
       dispatch(updateUserProfile({ id: user._id, name, email, password }))
     }
+  }
+
+  // toggle visiblity for password
+
+  const [showPassword, setShowPassword] = useState(false)
+
+  const handlePasswordToggle = () => {
+    setShowPassword(!showPassword)
+  }
+
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false)
+
+  const handleConfirmPasswordToggle = () => {
+    setShowConfirmPassword(!showConfirmPassword)
+  }
+
+  // clear input field for password
+
+  const handleClearPassword = () => {
+    setPassword('')
   }
 
   return (
@@ -57,43 +82,124 @@ const ProfileScreen = () => {
         {success && <Message variant='success'>Profile Updated</Message>}
         {loading && <Loader />}
         <Form onSubmit={submitHandler}>
-          <Form.Group controlId='name'>
-            <Form.Label>Name</Form.Label>
-            <Form.Control
-              type='name'
-              placeholder='Enter name'
-              value={name}
-              onChange={(e) => setName(e.target.value)}></Form.Control>
-          </Form.Group>
+          <label
+            for='name'
+            className='my-2'
+            style={{ fontWeight: 'bold', fontSize: '20px' }}>
+            Name:
+          </label>
+          <div className='input-group'>
+            <div className='form-div'>
+              <input
+                type='name'
+                placeholder='Enter name'
+                value={name}
+                id='name'
+                className='input-form'
+                onChange={(e) => setName(e.target.value)}
+              />
+            </div>
+          </div>
 
-          <Form.Group controlId='email'>
-            <Form.Label>Email Address</Form.Label>
-            <Form.Control
-              type='email'
-              placeholder='Enter email'
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}></Form.Control>
-          </Form.Group>
+          <label
+            for='email-input'
+            className='my-2'
+            style={{ fontWeight: 'bold', fontSize: '20px' }}>
+            Email Address:
+          </label>
+          <div className='input-group'>
+            <div className='form-div'>
+              <input
+                type='email'
+                placeholder='Enter email'
+                value={email}
+                id='email-input'
+                className='input-form'
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+          </div>
 
-          <Form.Group controlId='password'>
-            <Form.Label>Password</Form.Label>
-            <Form.Control
-              type='password'
-              placeholder='Enter password'
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}></Form.Control>
-          </Form.Group>
+          {/* Password Field */}
+          <label
+            for='password'
+            className='my-2'
+            style={{ fontWeight: 'bold', fontSize: '20px' }}>
+            Password:
+          </label>
+          <div
+            className='input-group'
+            style={{ backgroundColor: 'rgb(240, 239, 239)' }}>
+            <div className='form-div'>
+              <input
+                type={showPassword ? 'text' : 'password'}
+                placeholder='Enter password'
+                value={password}
+                id='password-input'
+                className='input-form'
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <div className='form-icon'>
+                <span onClick={handleClearPassword} className='mt-2'>
+                  {password && (
+                    <i
+                      className='fa-solid fa-circle-xmark'
+                      style={{ color: 'grey' }}></i>
+                  )}
+                </span>
+              </div>
+              <div className='form-icon'>
+                <span onClick={handlePasswordToggle} className='mt-2'>
+                  {showPassword ? (
+                    <i className='fa-regular fa-eye'></i>
+                  ) : (
+                    <i className='fa-regular fa-eye-slash'></i>
+                  )}
+                </span>
+              </div>
+            </div>
+          </div>
 
-          <Form.Group controlId='confirmPassword'>
-            <Form.Label>Confirm Password</Form.Label>
-            <Form.Control
-              type='password'
-              placeholder='Confirm password'
-              value={confirmPassword}
-              onChange={(e) =>
-                setConfirmPassword(e.target.value)
-              }></Form.Control>
-          </Form.Group>
+          {/* Re-enter Password Field */}
+
+          <label
+            for='confirmPassword'
+            className='my-2'
+            style={{ fontWeight: 'bold', fontSize: '20px' }}>
+            Confirm Password:
+          </label>
+          <div
+            className='input-group'
+            style={{ backgroundColor: 'rgb(240, 239, 239)' }}>
+            <div className='form-div'>
+              <input
+                type={showConfirmPassword ? 'text' : 'password'}
+                placeholder='Enter password'
+                value={confirmPassword}
+                id='confirmPassword'
+                className='input-form'
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              />
+              <div className='form-icon'>
+                <span onClick={handleClearPassword} className='mt-2'>
+                  {confirmPassword && (
+                    <i
+                      className='fa-solid fa-circle-xmark'
+                      style={{ color: 'grey' }}></i>
+                  )}
+                </span>
+              </div>
+              <div className='form-icon'>
+                <span onClick={handleConfirmPasswordToggle} className='mt-2'>
+                  {showConfirmPassword ? (
+                    <i className='fa-regular fa-eye'></i>
+                  ) : (
+                    <i className='fa-regular fa-eye-slash'></i>
+                  )}
+                </span>
+              </div>
+            </div>
+          </div>
 
           <Button type='submit' variant='primary' className='py-2 my-3'>
             Update
