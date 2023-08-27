5a7801c70dac19f0097f58a1deda6fb175a433b6
Ezerike Caleb Chikere <cezerike@gmail.com>
2023-08-27 11:13:20 +0100

    'editing shipping screen'

--- src/screens/ShippingScreen.js
+++ src/screens/ShippingScreen.js
@@ -4,6 +4,7 @@ import { Form, Button } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import FormContainer from '../components/FormContainer'
 import { saveShippingAddress } from '../actions/cartActions'
+import StateDropdown from '../components/StateDropdown'
 
 const ShippingScreen = () => {
   const cart = useSelector((state) => state.cart)
@@ -13,6 +14,14 @@ const ShippingScreen = () => {
   const [city, setCity] = useState(shippingAddress.city)
   const [postalCode, setPostalCode] = useState(shippingAddress.postalCode)
   const [country, setCountry] = useState(shippingAddress.country)
+
+  const nigerianStates = ['Abia', 'Adamawa', 'Akwa Ibom' /* ... and so on */]
+
+  const [selectedState, setSelectedState] = useState('')
+
+  const handleStateSelect = (event) => {
+    setSelectedState(event.target.value)
+  }
 
   const dispatch = useDispatch()
   let navigate = useNavigate()
@@ -35,7 +44,7 @@ const ShippingScreen = () => {
           </div>
         </div>
         <Form onSubmit={submitHandler}>
-          <label
+          {/* <label
             for='address'
             className='my-2'
             style={{ fontWeight: 'bold', fontSize: '20px' }}>
@@ -53,71 +62,109 @@ const ShippingScreen = () => {
                 onChange={(e) => setAddress(e.target.value)}
               />
             </div>
-          </div>
-
-          <label
-            for='city'
-            className='my-2'
-            style={{ fontWeight: 'bold', fontSize: '20px' }}>
-            City:
-          </label>
-          <div className='input-group'>
-            <div className='form-div'>
-              <input
-                type='text'
-                placeholder='Enter city'
-                value={city}
-                required
-                id='city'
-                className='input-form'
-                onChange={(e) => setCity(e.target.value)}
-              />
+          </div> */}
+          <div className='container'>
+            <label
+              for='address'
+              className='my-2'
+              style={{ fontWeight: 'bold', fontSize: '20px' }}>
+              <i className='fa-solid fa-location-dot'></i> Street address 1:
+            </label>
+            <div class='row'>
+              <div class='col-md-12'>
+                <textarea
+                  class='form-control'
+                  rows='2'
+                  style={{ width: '100%' }}></textarea>
+              </div>
             </div>
           </div>
 
-          <label
-            for='postalcode'
-            className='my-2'
-            style={{ fontWeight: 'bold', fontSize: '20px' }}>
-            Postal Code:
-          </label>
-          <div className='input-group'>
-            <div className='form-div'>
-              <input
-                type='text'
-                placeholder='Enter postal code'
-                value={postalCode}
-                required
-                id='postalcode'
-                className='input-form'
-                onChange={(e) => setPostalCode(e.target.value)}
-              />
+          <div className='container'>
+            <label
+              for='address'
+              className='my-2'
+              style={{ fontWeight: 'bold', fontSize: '20px' }}>
+              <i className='fa-solid fa-location-dot'></i> Street address 2:
+            </label>
+            <div class='row'>
+              <div class='col-md-12'>
+                <textarea
+                  class='form-control'
+                  rows='2'
+                  style={{ width: '100%' }}></textarea>
+              </div>
             </div>
           </div>
 
-          <label
-            for='country'
-            className='my-2'
-            style={{ fontWeight: 'bold', fontSize: '20px' }}>
-            Country:
-          </label>
-          <div className='input-group'>
-            <div className='form-div'>
-              <input
-                type='text'
-                placeholder='Enter country'
-                value={country}
-                required
-                id='country'
-                className='input-form'
-                onChange={(e) => setCountry(e.target.value)}
-              />
+          <div className='container'>
+            <label
+              for='city'
+              className='my-2'
+              style={{ fontWeight: 'bold', fontSize: '20px' }}>
+              <i className='fa-solid fa-city'></i> City:
+            </label>
+            <div className='input-group'>
+              <div className='form-div'>
+                <StateDropdown
+                  states={nigerianStates}
+                  onSelect={handleStateSelect}
+                  style={{ width: '300px' }}
+                />
+                {selectedState && <p>You selected: {selectedState}</p>}
+              </div>
             </div>
           </div>
 
-          <Button type='submit' variant='primary' className='mt-3'>
-            Continue
-          </Button>
+          <div className='container'>
+            <label
+              for='postalcode'
+              className='my-2'
+              style={{ fontWeight: 'bold', fontSize: '20px' }}>
+              Postal Code:
+            </label>
+            <div className='input-group'>
+              <div className='form-div'>
+                <input
+                  type='text'
+                  placeholder='Enter postal code'
+                  value={postalCode}
+                  required
+                  id='postalcode'
+                  className='input-form'
+                  onChange={(e) => setPostalCode(e.target.value)}
+                />
+              </div>
+            </div>
+          </div>
+
+          <div className='container'>
+            <label
+              for='country'
+              className='my-2'
+              style={{ fontWeight: 'bold', fontSize: '20px' }}>
+              Country:
+            </label>
+            <div className='input-group'>
+              <div className='form-div'>
+                <input
+                  type='text'
+                  placeholder='Enter country'
+                  value={country}
+                  required
+                  id='country'
+                  className='input-form'
+                  onChange={(e) => setCountry(e.target.value)}
+                />
+              </div>
+            </div>
+          </div>
+
+          <div className='container'>
+            <Button type='submit' variant='primary' className='mt-3'>
+              Continue
+            </Button>
+          </div>
         </Form>
       </FormContainer>
     </>
